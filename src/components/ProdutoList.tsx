290dafec2472a6d738416e40352d2236658f5d3f
KeyssonG <keyssoncardoso0@gmail.com>
2026-07-28 23:07:57 -0300

    fix: corrige classes CSS do grid de produtos (gridCardContainer/gridCard)

--- src/components/ProdutoList.tsx
+++ src/components/ProdutoList.tsx
@@ -106,11 +106,11 @@ const ProdutoList: React.FC<Props> = ({ onNovo, onEditar, onError }) => {
         </button>
       </div>
 
-      <div className={styles.gridCard}>
+      <div className={styles.gridCardContainer}>
         {listaProdutos.map(produto => {
           const estoqueStatus = getEstoqueStatus(produto);
           return (
-            <div key={produto.id_produto} className={styles.cardBadge}>
+            <div key={produto.id_produto} className={styles.gridCard}>
               <div className={styles.cardIcon}>
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M20.59 13.41l-7.17 7.17a2 2 0 0 1-2.83 0L2 12V2h10l8.59 8.59a2 2 0 0 1 0 2.82z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
